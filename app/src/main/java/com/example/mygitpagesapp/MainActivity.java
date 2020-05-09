9b99ff06211495de8ef57228469e68bf1b4679b4
yesjiyoung <rohjy77@gmail.com>
2020-05-10 00:53:07 +0900

    completed making MyGitpagesApp

--- app/src/main/java/com/example/mygitpagesapp/MainActivity.java
+++ app/src/main/java/com/example/mygitpagesapp/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.mygitpagesapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    /** private 설정*/
+    private WebView webView;
+    private WebSettings webSettings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById((R.id.webvw));
+        webView.setWebViewClient(new WebViewClient());
+
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://yesjiyoung.github.io/MyProfile_and_TeamProfile.github.io/");
+
+//        WebView myWebView = new WebView(activityContext);
+//        setContentView(myWebView);
+
+
     }
 }
